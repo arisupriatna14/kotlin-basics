6b4241629cbe4167c5e5753fd493ece08cdb5d89
arisupriatna14 <arisupriatna703@gmail.com>
2019-03-18 10:55:39 +0700

    add for loops using withIndex() and indices

--- src/basics/for_loop.kt
+++ src/basics/for_loop.kt
@@ -6,4 +6,14 @@ fun main(args: Array<String>) {
     for (item in fruits) {
         println("This is a $item")
     }
+
+    val arrayNumbers = intArrayOf(1, 3, 5, 7, 9)
+    // menggunakan withIndex()
+    for ((index, value) in arrayNumbers.withIndex()) {
+        println("Bilangan $value adalah index ke-$index")
+    }
+    // menggunakan indices()
+    for (index in arrayNumbers.indices) {
+        println("Bilangan ${arrayNumbers[index]} adalah index ke-$index")
+    }
 }
